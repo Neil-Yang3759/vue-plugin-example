c0ed1704135acf6ff9fd1ab6f613b08453c4e45a
Neil-Yang3759 <cloudyang3759@gmail.com>
2023-11-09 12:06:24 +0800

    hi

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+
+<template>
+  <div>
+    <h1>Demo Page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- <style lang="sass" type="text/sass" scoped></style> -->
